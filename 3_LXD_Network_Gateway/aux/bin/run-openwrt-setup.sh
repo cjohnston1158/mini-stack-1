542e38502148f549c4a8a6eeac87c77ceb1e8260
katamo <kat.morgan@braincraft.io>
2019-05-15 11:19:07 -1000

    update s

--- 3_LXD_Network_Gateway/aux/bin/run-openwrt-setup.sh
+++ 3_LXD_Network_Gateway/aux/bin/run-openwrt-setup.sh
@@ -7,17 +7,17 @@ run_backup () {
 }
 
 pull_config_files () {
-wget -O /etc/config/dhcp      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/dhcp
-wget -O /etc/config/dropbear  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/dropbear
-wget -O /etc/config/firewall  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/firewall
-wget -O /etc/config/luci      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/luci
-wget -O /etc/config/network   https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/network
-wget -O /etc/config/rpcd      https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/rpcd
-wget -O /etc/config/squid     https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/squid
-wget -O /etc/config/system    https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/system
-wget -O /etc/config/ucitrack  https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/ucitrack
-wget -O /etc/config/uhtpd     https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/config/uhttpd
-wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/KathrynMorgan/mini-stack/master/3_LXD_Network_Gateway/aux/squid.conf 
+wget -O /etc/config/dhcp      https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/dhcp
+wget -O /etc/config/dropbear  https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/dropbear
+wget -O /etc/config/firewall  https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/firewall
+wget -O /etc/config/luci      https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/luci
+wget -O /etc/config/network   https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/network
+wget -O /etc/config/rpcd      https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/rpcd
+wget -O /etc/config/squid     https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/squid
+wget -O /etc/config/system    https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/system
+wget -O /etc/config/ucitrack  https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/ucitrack
+wget -O /etc/config/uhtpd     https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/config/uhttpd
+wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/containercraft/mini-stack/master/3_LXD_Network_Gateway/aux/squid.conf 
 }
 
 run_squid_config () {
