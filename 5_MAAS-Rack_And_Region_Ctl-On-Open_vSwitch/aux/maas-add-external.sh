e00dd1bdf9ae94875269065c426e0e7153d67285
katamo <kat.morgan@braincraft.io>
2019-06-15 23:52:39 -0600

    tweaks

--- 5_MAAS-Rack_And_Region_Ctl-On-Open_vSwitch/aux/maas-add-external.sh
+++ 5_MAAS-Rack_And_Region_Ctl-On-Open_vSwitch/aux/maas-add-external.sh
@@ -1,29 +1,23 @@
 #!/bin/bash -x
 #################################################################################
-source /etc/ccio/mini-stack/profile
-
-#################################################################################
-add_iface () {
-echo ">  Attaching 'external' network on interface 'eth1'..."
-lxc network attach external maasctl eth1 eth1
-lxc exec maasctl -- /bin/bash -c 'netplan apply --debug'
-}
-
-stage_config () {
+# Stage maas network config routine
+stage_setup () {
 echo ">  Staging Configuration Script ..."
-cat <<EOF >/tmp/run_maas_setup
+cat <<EOF >/tmp/run-maas-setup
 #!/bin/bash 
-
+##################################################################################
 echo ">  Running maas network configuration ..."  
-set -x
-
+##################################################################################
+# MAAS Login Admin
 run_maas_login () {
 login-maas-cli
-login_CODE="$?"
-[[ ${login_CODE} == "0" ]] || echo "Login Failed"
-[[ ${login_CODE} == "0" ]] || exit 1
+login_CODE="\$?"
+[[ \${login_CODE} == "0" ]] || echo "Login Failed"
+[[ \${login_CODE} == "0" ]] || exit 1
 }
 
+##################################################################################
+# Find Rack ID
 find_maas_rack_id () {
 primary_RACK=\$(maas admin rack-controllers read \
                                 | jq ".[] | {system_id:.system_id}" \
@@ -31,54 +25,73 @@ primary_RACK=\$(maas admin rack-controllers read \
               )
 }
 
+##################################################################################
+# Run MAAS Network Config
 run_maas_setup () {
 
-maas admin spaces create name=external
-maas admin fabric update 1 name=external
-maas admin subnet update 1 name=external
-maas admin vlan update external 0 name=external space=external
+  # Configue 'External' Network
+  maas admin spaces create name=external
+  maas admin fabric update 1 name=external
+  maas admin subnet update 1 name=external
+  maas admin vlan update external 0 name=external space=external
 
 }
 
-run_maas_login
-find_maas_rack_id
-run_maas_setup
+##################################################################################
+# Reload Region & Rack Services
+service_reload () {
 
-echo "Finished run_maas_setup at \$(date)"
+  systemctl restart maas-regiond
+  sleep 5
+  systemctl restart maas-rackd
+
+}
+
+##################################################################################
+run () {
+
+  run_maas_login
+  find_maas_rack_id
+  run_maas_setup
+  echo "Finished run-maas-setup at \$(date)"
+
+}
 
 EOF
 
-chmod +x /tmp/run_maas_setup
+chmod +x /tmp/run-maas-setup
 }
 
-load_config () {
-echo ">  Loading maas configuration script"
-lxc file push /tmp/run_maas_setup maasctl/bin/run_maas_setup
+##################################################################################
+add_iface () {
+
+  echo ">  Attaching 'external' network on interface 'eth1'..."
+
+  lxc network attach external maasctl eth1 eth1
+  lxc exec maasctl -- /bin/bash -c 'netplan apply --debug'
+
 }
 
-restart_maasctl () {
-echo '>  Restarting MaasCTL'
-lxc stop maasctl
-sleep 1
-ovs-clear
-lxc start maasctl
-sleep 1
-lxc list
-}
-
-exec_config () {
-echo ">  Configuring 'external' network bridge..."
-lxc exec maasctl run_maas_setup
-lxc exec maasctl run_maas_setup
-}
-
+##################################################################################
 run () {
-add_iface
-stage_config
-load_config
-restart_maasctl
-exec_config
+
+  # Load Profile
+  source /etc/ccio/mini-stack/profile
+
+  # Attach 'External' Iface 'eth1'
+  add_iface
+
+  # Stage Config
+  stage_setup
+
+  # Load Config
+  echo ">  Loading maas configuration script"
+  lxc file push /tmp/run-maas-setup maasctl/bin/run-maas-setup
+
+  # Run Config
+  echo ">  Configuring 'external' network bridge..."
+  lxc exec maasctl run-maas-setup
+
 }
 
 run
-
--- 5_MAAS-Rack_And_Region_Ctl-On-Open_vSwitch/aux/run-setup-maas.sh
+++ 5_MAAS-Rack_And_Region_Ctl-On-Open_vSwitch/aux/run-setup-maas.sh
@@ -1,58 +1,104 @@
-source /etc/ccio/mini-stack/profile
+#!/bin/bash
+##################################################################################
+# Stage maas network config routine
+stage_setup () {
+echo ">  Staging Configuration Script ..."
 cat <<EOF >/tmp/run-maas-setup
 #!/bin/bash 
-
+##################################################################################
+upstream_DNS="8.8.8.8"
+echo ">  Running maas network configuration ..."  
+##################################################################################
+# MAAS Login Admin
 run_maas_login () {
-login-maas-cli
-login_CODE="$?"
-[[ \${login_CODE} == "0" ]] || echo "Login Failed"
-[[ \${login_CODE} == "0" ]] || exit 1
+
+  login-maas-cli
+  login_CODE="\$?"
+  [[ \${login_CODE} == "0" ]] || echo "Login Failed"
+  [[ \${login_CODE} == "0" ]] || exit 1
+
 }
 
+##################################################################################
+# Find Rack ID
 find_maas_rack_id () {
-primary_RACK=\$(maas admin rack-controllers read \
+
+  primary_RACK=\$(maas admin rack-controllers read \
                                 | jq ".[] | {system_id:.system_id}" \
                                 | awk -F'[",]' '/system_id/{print \$4}' \
               )
-}
-
-run-maas-setup () {
-maas admin maas set-config name=maas_name value=maasctl
-maas admin maas set-config name=upstream_dns value=8.8.8.8
-maas admin maas set-config name=enable_third_party_drivers value=true
-maas admin maas set-config name=disk_erase_with_secure_erase value=false
-maas admin maas set-config name=kernel_opts value='debug console=ttyS0,38400n8 console=tty0 intel_iommu=on iommu=pt kvm_intel.nested=1 net.ifnames=0 biosdevname=0 pci=noaer'
-
-maas admin spaces create name=internal
-maas admin spaces create name=external
-maas admin fabric update 0 name=internal
-maas admin subnet update 1 name=untagged-internal gateway_ip="${ministack_SUBNET}.1" dns_servers="${ministack_SUBNET}.10"
-maas admin ipranges create type=dynamic start_ip=${ministack_SUBNET}.100 end_ip=${ministack_SUBNET}.240
-maas admin vlan update internal 0 name=internal space=internal
-maas admin vlan update internal untagged dhcp_on=True primary_rack=\${primary_RACK}
 
 }
 
-run_maas_login
-find_maas_rack_id
-run-maas-setup
+##################################################################################
+# Run MaaS Network Config
+run_maas_setup () {
 
-rm /bin/run-maas-setup
-echo "Finished run-maas-setup at \$(date)"
+  # MAAS Controller Config
+  maas admin maas set-config name=maas_name value=maasctl
+  maas admin maas set-config name=upstream_dns value=\${upstream_DNS}
+  maas admin maas set-config name=enable_third_party_drivers value=true
+  maas admin maas set-config name=disk_erase_with_secure_erase value=false
 
-#################################################################################
-# WIP
-#maas admin pods create type=virsh name=mini-stack.maas power_address=qemu+ssh://root@mini-stack/system cpu_over_commit_ratio=10 memory_over_commit_ratio=10
-#maas admin dnsresource-records update name=mini-stack domain=maas rrdata=${ministack_SUBNET}.2 rrtype=cname ip_addresses=${ministack_SUBNET}.2
-#maas admin devices create hostname=mini-stack domain=maas mac_addresses=02:17:77:61:55:7b ip_addresses=${ministack_SUBNET}.2 ip_address=${ministack_SUBNET}.2
+  # Set Kernel Parameter
+  maas admin maas set-config name=kernel_opts \
+    value='debug console=ttyS0,38400n8 console=tty0 intel_iommu=on iommu=pt kvm_intel.nested=1 net.ifnames=0 biosdevname=0 pci=noaer'
 
-systemctl restart maas-regiond
-sleep 5
-systemctl restart maas-rackd
-rm /bin/run-maas-setup
+  # Create Spaces
+  maas admin spaces create name=internal
+  maas admin spaces create name=external
+
+  # Configure 'Internal' Network
+  maas admin fabric update 0 name=internal
+  maas admin subnet update 1 name=untagged-internal \
+    gateway_ip="${ministack_SUBNET}.1" dns_servers="${ministack_SUBNET}.10"
+  maas admin ipranges create type=dynamic \
+    start_ip=${ministack_SUBNET}.100 end_ip=${ministack_SUBNET}.240
+  maas admin vlan update internal 0 name=internal space=internal
+  maas admin vlan update internal untagged dhcp_on=True primary_rack=\${primary_RACK}
+
+}
+
+##################################################################################
+service_reload () {
+
+  systemctl restart maas-regiond
+  sleep 5
+  systemctl restart maas-rackd
+
+}
+
+##################################################################################
+run () {
+
+  run_maas_login
+  find_maas_rack_id
+  run_maas_setup
+  service_reload 
+  echo "Finished run-maas-setup at \$(date)"
+
+}
+
+run
 EOF
-
 chmod +x /tmp/run-maas-setup
-lxc file push /tmp/run-maas-setup maasctl/bin/run-maas-setup
-lxc snapshot maasctl pre-run-setup
-lxc exec maasctl run-maas-setup
+}
+
+##################################################################################
+run () {
+  
+  # Load Profile
+  source /etc/ccio/mini-stack/profile
+
+  # Stage Config
+  stage_setup
+
+  # Load Config
+  lxc file push /tmp/run-maas-setup maasctl/bin/run-maas-setup
+
+  # Run Config
+  lxc exec maasctl run-maas-setup
+
+}
+
+run
